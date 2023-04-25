0ba995a10edf992d118efb5e478a723f5f13f6f3
Fejmus <131805608+Fejmus@users.noreply.github.com>
2023-04-25 22:02:21 +0200

    Add files via upload

---
+++ skrypt4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ "$1" == "--date" ]] || [[ "$1" == "-d" ]]; then
+    echo "Dzisiaj jest $(date +%Y-%m-%d)"
+elif [[ "$1" == "--logs" ]] || [[ "$1" == "-l" ]]; then
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        for i in $(seq 1 "$2"); do
+            filename="log${i}.txt"
+            echo "Nazwa pliku: $filename" >> "$filename"
+            echo "Nazwa skryptu: $0" >> "$filename"
+            echo "Data utworzenia: $(date)" >> "$filename"
+        done
+    else
+        echo "Nieprawidłowy argument wywołania"
+    fi
+elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+    echo "Użycie: skrypt.sh --date lub -d|--logs lub -l [N]"
+    echo "--date: Wyświetla dzisiejszą datę w formacie YYYY-MM-DD."
+    echo "--logs [N]: Tworzy N plików logu o nazwie logx.txt (dla x od 1 do N) z wpisem o nazwie, nazwie skryptu i dacie utworzenia."
+else
+    echo "Nieprawidłowe argumenty wywołania."
+fi
